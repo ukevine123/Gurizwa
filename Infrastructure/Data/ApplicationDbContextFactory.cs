c68108e934cc2d5e709ba5c4811c61c2d2b7aae2
ukevine123 <ukevine239@gmail.com>
2026-06-30 17:20:53 +0200

    Merged unrelated histories from remote Kevine branch

--- Infrastructure/Data/ApplicationDbContextFactory.cs
+++ Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -10,7 +10,7 @@ namespace Infrastructure.Data
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
 
             optionsBuilder.UseSqlServer(
-                "Server=(localdb)\\mssqllocaldb;Database=DigitalLoanPlatform;Trusted_Connection=True;MultipleActiveResultSets=true",
+                "Server=TERANOS-V01;Database=DigitalLoanPlatForm;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True",
                 sqlOptions => sqlOptions.MigrationsAssembly("Infrastructure"));
 
             return new ApplicationDbContext(optionsBuilder.Options);
